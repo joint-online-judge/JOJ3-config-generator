6481f2d17ed97446084a319dfa099a20e89f0f8a
Nuvole <jon-lee@sjtu.edu.cn>
2024-10-18 01:08:35 +0800

    feat: task setup

--- lib/task.py
+++ lib/task.py
@@ -2,3 +2,23 @@ import yaml
 import json
 import os
 import tomllib
+from lib.frame import stage_frame
+
+def stage_distribute(main_json): # input should be the whole json file
+    json = main_json
+    with open("../toml/task_complex.toml", "rb") as f:
+        config = tomllib.load(f)
+    
+    stage_meta = "name"
+    headers = list(config.keys())
+    for _, header in enumerate(headers):
+        if stage_meta in config[header]:
+            json['stages'].append(build_json(header, config))
+        else: 
+            continue
+    return json
+
+def build_json(key, toml_file):
+    json = stage_frame()
+    
+    return json
--- src/main.py
+++ src/main.py
@@ -3,13 +3,15 @@ import tomllib
 sys.path.append("../")
 from lib.repo import *
 from lib.frame import *
-from lib.compile import *
 from lib.task import *
 
 def main():    
-    healthcheck_json = get_healthcheck()
+    repo_json = get_healthcheck()
     frame = get_frame()
-    return 0
+    frame['stages'] = repo_json
+    print(frame)
+    stage_distribute(frame)
+    return frame
 
 if __name__ == "__main__":
     main()
