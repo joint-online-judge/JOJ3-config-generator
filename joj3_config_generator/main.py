c3d893d2688a00e9542a75e520de6eb5d3520835
Boming Zhang <bomingzh@sjtu.edu.cn>
2024-10-19 00:46:47 -0400

    feat: add inquirer example

--- joj3_config_generator/main.py
+++ joj3_config_generator/main.py
@@ -1,3 +1,4 @@
+import inquirer
 import typer
 
 from joj3_config_generator.utils.logger import logger
@@ -5,26 +6,26 @@ from joj3_config_generator.utils.logger import logger
 app = typer.Typer(add_completion=False)
 
 
-@app.callback()
-def callback() -> None:
+@app.command()
+def create() -> None:
     """
-    Awesome Portal Gun
+    Create a new JOJ3 config file
     """
+    logger.info("Creating")
+    questions = [
+        inquirer.List(
+            "size",
+            message="What size do you need?",
+            choices=["Jumbo", "Large", "Standard", "Medium", "Small", "Micro"],
+        ),
+    ]
+    answers = inquirer.prompt(questions)
+    logger.info(answers)
 
 
 @app.command()
-def shoot() -> None:
+def convert() -> None:
     """
-    Shoot the portal gun
+    Convert a JOJ1 config file to JOJ3 config file
     """
-    typer.echo("Shooting portal gun")
-    logger.info("Shooting portal gun")
-
-
-@app.command()
-def load() -> None:
-    """
-    Load the portal gun
-    """
-    typer.echo("Loading portal gun")
-    logger.info("Loading portal gun")
+    logger.info("Converting")
