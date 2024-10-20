228488cbf275d0ca9e201e983203e477e68aa708
Boming Zhang <bomingzh@sjtu.edu.cn>
2024-10-20 01:54:55 -0400

    feat: repo & task config model

--- tests/example_test.py
+++ tests/example_test.py
@@ -3,10 +3,15 @@
 
 from typing import Any
 
+import pytest
 
+
+@pytest.mark.xfail(strict=True)
 def test_generate() -> None:
     generate = lambda x: x  # TODO: real generate function imported
     data_input: dict[Any, Any] = {}  # TODO: load real input from some file
     data_output: dict[Any, Any] = generate(data_input)
-    expected_output: dict[Any, Any] = {}  # TODO: load real output from some file
+    expected_output: dict[Any, Any] = {
+        "a": "b"
+    }  # TODO: load real output from some file
     assert data_output == expected_output
