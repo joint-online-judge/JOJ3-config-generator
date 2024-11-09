cb34dced5006803b3e68c29558e260ec2eb3a164
Nuvole <jon-lee@sjtu.edu.cn>
2024-11-09 14:54:39 +0800

    fix: type annotation

--- joj3_config_generator/lib/task.py
+++ joj3_config_generator/lib/task.py
@@ -1,12 +1,12 @@
 import shlex
-from typing import Any, Dict, Tuple
+from typing import Tuple
 
 import rtoml
 
 from joj3_config_generator.models import joj1, repo, result, task
 
 
-def remove_nulls(d: Dict[str, Any]) -> Dict[str, Any]:
+def remove_nulls(d: result.Config) -> result.Config:
     if isinstance(d, dict):
         return {k: remove_nulls(v) for k, v in d.items() if v is not None}
     elif isinstance(d, list):
