566a64465f38df49803e46b2adeab54fbb0e4174
Nuvole <jon-lee@sjtu.edu.cn>
2024-11-12 23:20:26 +0800

    fix: default value

--- joj3_config_generator/models/result.py
+++ joj3_config_generator/models/result.py
@@ -8,7 +8,7 @@ class CmdFile(BaseModel):
     content: Optional[str] = None
     file_id: Optional[str] = Field(None, serialization_alias="fileId")
     name: Optional[str] = None
-    max: Optional[int] = 4 * 1024 * 1024
+    max: Optional[int] = 40 * 1024 * 1024
     symlink: Optional[str] = None
     stream_in: bool = Field(False, serialization_alias="streamIn")
     stream_out: bool = Field(False, serialization_alias="streamOut")
@@ -21,10 +21,10 @@ class Cmd(BaseModel):
     stdin: Optional[CmdFile] = CmdFile(content="")
     stdout: Optional[CmdFile] = CmdFile(name="stdout", max=4 * 1024)
     stderr: Optional[CmdFile] = CmdFile(name="stderr", max=4 * 1024)
-    cpu_limit: int = Field(4 * 1000000000, serialization_alias="cpuLimit")
+    cpu_limit: int = Field(4 * 1000000000000, serialization_alias="cpuLimit")
     real_cpu_limit: int = Field(0, serialization_alias="realCpuLimit")
-    clock_limit: int = Field(8 * 1000000000, serialization_alias="clockLimit")
-    memory_limit: int = Field(4 * 1024 * 1024, serialization_alias="memoryLimit")
+    clock_limit: int = Field(8 * 1000000000000, serialization_alias="clockLimit")
+    memory_limit: int = Field(80 * 1024 * 1024, serialization_alias="memoryLimit")
     stack_limit: int = Field(0, serialization_alias="stackLimit")
     proc_limit: int = Field(50, serialization_alias="procLimit")
     cpu_rate_limit: int = Field(0, serialization_alias="cpuRateLimit")
@@ -48,11 +48,11 @@ class OptionalCmd(BaseModel):
     stdin: Optional[CmdFile] = None
     stdout: Optional[CmdFile] = None
     stderr: Optional[CmdFile] = None
-    cpu_limit: Optional[int] = Field(4 * 1000000000, serialization_alias="cpuLimit")
+    cpu_limit: Optional[int] = Field(4 * 1000000000000, serialization_alias="cpuLimit")
     real_cpu_limit: Optional[int] = Field(None, serialization_alias="realCpuLimit")
-    clock_limit: Optional[int] = Field(8 * 1000000000, serialization_alias="clockLimit")
+    clock_limit: Optional[int] = Field(8 * 1000000000000, serialization_alias="clockLimit")
     memory_limit: Optional[int] = Field(
-        4 * 1024 * 1024, serialization_alias="memoryLimit"
+        80 * 1024 * 1024, serialization_alias="memoryLimit"
     )
     stack_limit: Optional[int] = Field(None, serialization_alias="stackLimit")
     proc_limit: Optional[int] = Field(50, serialization_alias="procLimit")
