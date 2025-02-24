80e2bf6bad445cd6b7c0bc03d37e7bbbe8e7cc7d
jon-lee <jon-lee@sjtu.edu.cn>
2025-02-24 22:29:23 +0800

    feat: small testcases for parsers

--- tests/convert/test_convert_cases.py
+++ tests/convert/test_convert_cases.py
@@ -3,3 +3,23 @@ from tests.convert.utils import load_case
 
 def test_basic() -> None:
     load_case("basic")
+
+
+def test_clang_tidy() -> None:
+    load_case("clang-tidy")
+
+
+def test_cppcheck() -> None:
+    load_case("cppcheck")
+
+
+def test_cpplint() -> None:
+    load_case("cpplint")
+
+
+def test_diff() -> None:
+    load_case("diff")
+
+
+def test_keyword() -> None:
+    load_case("keyword")
