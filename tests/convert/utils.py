3b40bee13cf3be46712a97fff7021bfecf08aeed
Boming Zhang <bomingzh@sjtu.edu.cn>
2025-03-04 15:23:46 -0500

    test: use tests/convert as root

--- tests/convert/utils.py
+++ tests/convert/utils.py
@@ -11,15 +11,19 @@ from joj3_config_generator.models import repo, task
 def read_convert_files(
     case_name: str,
 ) -> Tuple[repo.Config, task.Config, Dict[str, Any]]:
-    root = Path(__file__).resolve().parent / case_name
-    repo_toml_path = root / "repo.toml"
+    root = Path(__file__).resolve().parent
+    repo_toml_path = root / case_name / "repo.toml"
     repo_toml = repo_toml_path.read_text() if repo_toml_path.exists() else ""
-    task_toml_path = root / "task.toml"
+    task_toml_path = root / case_name / "task.toml"
     task_toml = task_toml_path.read_text() if task_toml_path.exists() else ""
-    result = json.loads((root / "task.json").read_text())
+    result = json.loads((root / case_name / "task.json").read_text())
     return (
-        repo.Config(root=root, **rtoml.loads(repo_toml)),
-        task.Config(root=root, **rtoml.loads(task_toml)),
+        repo.Config(
+            root=root, path=repo_toml_path.relative_to(root), **rtoml.loads(repo_toml)
+        ),
+        task.Config(
+            root=root, path=task_toml_path.relative_to(root), **rtoml.loads(task_toml)
+        ),
         result,
     )
 
