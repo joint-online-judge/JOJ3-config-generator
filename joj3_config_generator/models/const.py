305663538dc285dab9ff666e1611f19f62ca36e7
Boming Zhang <bomingzh@sjtu.edu.cn>
2025-03-21 19:10:32 -0400

    feat: higher mem limit for clang

--- joj3_config_generator/models/const.py
+++ joj3_config_generator/models/const.py
@@ -3,7 +3,7 @@ from pathlib import Path
 from joj3_config_generator.models.common import Memory, Time
 
 DEFAULT_CPU_LIMIT = Time("1s")
-DEFAULT_MEMORY_LIMIT = Memory("128m")
+DEFAULT_MEMORY_LIMIT = Memory("256m")
 DEFAULT_FILE_LIMIT = Memory("32m")
 
 JOJ3_CONFIG_ROOT = Path("/home/tt/.config/joj")
